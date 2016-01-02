6baba117ea7370338b8b07880517967160e5548f
mcaravind <blogsooner@gmail.com>
2016-01-02 11:39:10 +0530

    Adding basic classes

---
+++ TextMinion/Parser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextMinion
+{
+    public class Parser
+    {
+        public List<string> GetSentences(string input)
+        {
+            return new List<string>();
+        }
+    }
+}
