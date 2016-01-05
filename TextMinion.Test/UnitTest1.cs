582ebb9df43bf8bb6dbff77344345ee652eb49a6
mcaravind <blogsooner@gmail.com>
2016-01-05 20:10:59 +0530

    Adding test project

---
+++ TextMinion.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextMinion;
+
+namespace TextMinion.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            SentenceTokenizer st = new SentenceTokenizer();
+            var sentences = st.GetSentences("This is a test sentence. In addition there is a second sentence.");
+            Assert.AreEqual(sentences.Count, 0);
+        }
+    }
+}
