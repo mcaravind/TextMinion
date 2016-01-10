fc95a9b996f4472b033b2a8bdf95e7503c2f33cc
mcaravind <blogsooner@gmail.com>
2016-01-10 08:13:26 +0530

    Added wordtokenizer

---
+++ TextMinion.Test/WordTokenizerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TextMinion.Test
+{
+    [TestClass]
+    public class WordTokenizerTest
+    {
+        [TestMethod]
+        public void VerifySingleSentence()
+        {
+            string input =
+                "Basically, this issue is not restricted to NVidia GPUs or specific operating systems - This can be reproduced on Windows, Linux and OSX. Basically the concept of memory safety does not exist in the gpu space - which is the reason why the webgl standard is so strict about always zeroing buffers.";
+            var wordList = WordTokenizer.GetWords(input);
+            Assert.IsTrue(wordList.Count>0);
+        }
+    }
+}
--- TextMinion/WordTokenizer.cs
+++ TextMinion/WordTokenizer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace TextMinion
@@ -11,6 +12,9 @@ namespace TextMinion
         public static List<string> GetWords(string input)
         {
             var wordList = new List<string>();
+            //replace all non alpha-numeric with space
+            var replaced = Regex.Replace(input, "[^0-9A-Za-z]", " ");
+            wordList = replaced.Split(new[]{' '}, StringSplitOptions.RemoveEmptyEntries).ToList();
             return wordList;
         } 
     }
