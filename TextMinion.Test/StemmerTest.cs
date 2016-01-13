cf105498496b7d19b15dafe59217af214842f815
mcaravind <blogsooner@gmail.com>
2016-01-13 21:12:17 +0530

    Adding stemming

---
+++ TextMinion.Test/StemmerTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TextMinion.Test
+{
+    [TestClass]
+    public class StemmerTest
+    {
+        [TestMethod]
+        public void Verify_Basic()
+        {
+            var input = "caresses";
+            var stemmed = Stemmer.GetStem(input);
+            Assert.AreEqual(stemmed,"caress");
+        }
+    }
+}
