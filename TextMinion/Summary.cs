10149564fc373e4b8b980274837d77071d021fcd
mcaravind <blogsooner@gmail.com>
2016-01-15 18:33:20 +0530

    Summary

---
+++ TextMinion/Summary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextMinion
+{
+    public static class Summary
+    {
+        public static List<string> GetSummary(string input)
+        {
+            var summarySentences = new List<string>();
+            return summarySentences;
+        } 
+    }
+}
