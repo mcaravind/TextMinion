da17d387ed8253e6c6244923eb7bda84d80149bf
mcaravind <blogsooner@gmail.com>
2016-01-21 14:46:58 +0530

    Defining classes for grammar rules

---
+++ TextMinion/Model/BinaryRule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextMinion.Model
+{
+    class BinaryRule
+    {
+        public Symbol LHS { get; set; }
+        public Symbol RHS1 { get; set; }
+        public Symbol RHS2 { get; set; }
+    }
+}
---
+++ TextMinion/Model/Symbol.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextMinion
+{
+    public class Symbol
+    {
+        public string Value { get; set; }
+    }
+}
