e781a541d876e4ba794fb4ecab4458c87c7e1e06
mcaravind <blogsooner@gmail.com>
2016-01-25 21:09:51 -0800

    Adding BinaryTree class

--- TextMinion/Grammar.cs
+++ TextMinion/Grammar.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TextMinion.Model;
 
 namespace TextMinion
 {
@@ -12,12 +13,13 @@ namespace TextMinion
         {
             //first get the list of words
             List<string> wordList = WordTokenizer.GetWords(sentence);
-            //
+            //send the word list to CKY to get a parse tree
+            BinaryTreeNode btn = CKY(wordList);
         }
 
-        static void CKY(List<string> wordList)
+        static BinaryTreeNode CKY(List<string> wordList)
         {
-            
+            return new BinaryTreeNode();
         }
     }
 }
---
+++ TextMinion/Model/BinaryTreeNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextMinion.Model
+{
+    public class BinaryTreeNode
+    {
+        public BinaryTreeNode LeftNode { get; set; }
+        public BinaryTreeNode RightNode { get; set; }
+        public string Value { get; set; }
+        public POS PosTag { get; set; }
+    }
+}
